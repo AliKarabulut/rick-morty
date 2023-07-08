cfb3fa50cdcadd5d35dc862d870f0694618e43aa
Ali Karabulut <56300204+AliKarabulut@users.noreply.github.com>
2023-07-08 06:46:48 +0300

    Sleeep

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -62,7 +62,7 @@ const getData = async (id) => {
 };
 
 const CharacterInfo = async ({ params }) => {
-  const data = async getData(params.id);
+  const data = await getData(params.id);
 
   if (!data || !data.character || data.error) {
     notFound();
