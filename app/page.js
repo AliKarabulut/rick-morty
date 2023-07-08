4217b7cf998a35f980d1ccb70d31f17053fe0280
Ali Karabulut <a.kbt6666@gmail.com>
2023-07-09 02:09:52 +0300

    search query and key fix for image bug

--- app/page.js
+++ app/page.js
@@ -4,9 +4,14 @@ import PageNumber from "@/components/pagination";
 import { getClient } from "@/lib/client";
 import { gql } from "@apollo/client";
 import { Fragment } from "react";
-import {notFound} from "next/navigation" 
+import { notFound } from "next/navigation";
 const Page = async ({ searchParams }) => {
-  const params = parseInt(searchParams?.page || 1, 10);
+  console.log(searchParams.page);
+  const params = isNaN(Number(searchParams.page))
+    ? 1
+    : Number(searchParams.page);
+  console.log(params);
+
   const query = gql`
     query Query($page: Int) {
       characters(page: $page) {
@@ -28,12 +33,14 @@ const Page = async ({ searchParams }) => {
     query,
     variables: { page: params },
   });
-if(data.characters.results.length === 0){notFound()}
+  if (data.characters.results.length === 0) {
+    notFound();
+  }
   return (
     <Fragment>
       <main className="grid gap-8 justify-items-center lg:px-16 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">
         {data.characters.results.map((character) => (
-          <CharacterCardWrapper character={character}>
+          <CharacterCardWrapper character={character} key={character.id + 1000}>
             <CharacterCard character={character} key={character.id} />
           </CharacterCardWrapper>
         ))}
